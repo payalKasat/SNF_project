e4ae93d5c35fa9828925bc1d251eaed74e25a37a
ashwini kasat <ashwini.kasat@gmail.com>
2018-03-05 22:38:04 -0800

    working on what we are section video

---
+++ script2.js
@@ -0,0 +1,28 @@
+/* Created by Ashwini 3/4/2018 */
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#home_page']").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+        $(".nav-tabs a").click(function(){
+            $(this).tab('show');
+        });
+});
